a4318676ee77b606adf69aea1029772c079350ab
augustus.lau.cn@gmail.com <augustus.lau.cn@gmail.com>
2021-09-08 08:50:53 +0800

    首次提交，登陆联通

---
+++ src/enums/roleEnum.ts
@@ -0,0 +1,8 @@
+export enum RoleEnum {
+  // super admin
+  SUPER = 'SYSTEM_ADMIN',
+  // 租户管理员
+  TENANT_ADMIN = 'TENANT_ADMIN',
+  // tester
+  TEST = 'test',
+}
---
+++ src/router/routes/index.ts
@@ -0,0 +1,50 @@
+import type { AppRouteRecordRaw, AppRouteModule } from '/@/router/types';
+
+import { PAGE_NOT_FOUND_ROUTE, REDIRECT_ROUTE } from '/@/router/routes/basic';
+
+import { mainOutRoutes } from './mainOut';
+import { PageEnum } from '/@/enums/pageEnum';
+import { t } from '/@/hooks/web/useI18n';
+
+const modules = import.meta.globEager('./modules/**/*.ts');
+
+const routeModuleList: AppRouteModule[] = [];
+
+Object.keys(modules).forEach((key) => {
+  const mod = modules[key].default || {};
+  const modList = Array.isArray(mod) ? [...mod] : [mod];
+  routeModuleList.push(...modList);
+});
+// 获取当前所有的菜单
+export const asyncRoutes = [PAGE_NOT_FOUND_ROUTE, ...routeModuleList];
+export const RootRoute: AppRouteRecordRaw = {
+  path: '/',
+  name: 'Root',
+  redirect: PageEnum.BASE_HOME,
+  meta: {
+    title: 'Root',
+  },
+};
+
+export const LoginRoute: AppRouteRecordRaw = {
+  path: '/login',
+  name: 'Login',
+  component: () => import('/@/views/sys/login/Login.vue'),
+  meta: {
+    title: t('routes.basic.login'),
+  },
+};
+
+// Basic routing without permission
+export const basicRoutes = [
+  // 登陆地址路由
+  LoginRoute,
+  // 根路由，用户首页
+  RootRoute,
+  // 位于主风格外的页面，用户构建两种风格的界面风格
+  ...mainOutRoutes,
+  // 跳转路由
+  REDIRECT_ROUTE,
+  // 404页面
+  PAGE_NOT_FOUND_ROUTE,
+];
